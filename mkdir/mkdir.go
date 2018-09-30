89fcdd50f08cd841b233866a4ca0048014a000d1
Kevin Dubrulle <kevin.dubrulle@gmail.com>
2018-09-30 18:42:56 +0200

    Impletment

--- mkdir/mkdir.go
+++ mkdir/mkdir.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/DubrulleKevin/gosix/common"
 )
+
+func chmodAll(path string, mode os.FileMode) {
+	var workingPath string
+
+	for i, el := range strings.Split(path, "/") {
+		if i > 0 {
+			workingPath += "/"
+		}
+		workingPath += el
+		err := os.Chmod(workingPath, mode)
+		common.PanicIfError(err)
+	}
+}
 
 func main() {
 	pFlagPtr := flag.Bool("p", false, "Create any missing intermediate pathname components.")
@@ -21,17 +34,20 @@ func main() {
 		fileMode64, err := strconv.ParseUint(*mFlagPtr, 8, 32)
 		fileMode = os.FileMode(fileMode64)
 		common.PanicIfError(err)
-		fmt.Println(fileMode)
-	} else {
-		fileMode = 0777
 	}
 
 	for _, arg := range args {
 		var err error
 		if *pFlagPtr {
-			err = os.MkdirAll(arg, fileMode)
+			err = os.MkdirAll(arg, 0777)
+			if fileMode != 0 {
+				chmodAll(arg, fileMode)
+			}
 		} else {
-			err = os.Mkdir(arg, fileMode)
+			err = os.Mkdir(arg, 0777)
+			if fileMode != 0 {
+				os.Chmod(arg, fileMode)
+			}
 		}
 		common.PanicIfError(err)
 	}
