6f97c0ffccf5672e01d0d050005bb289834a2023
Kevin Dubrulle <kevin.dubrulle@gmail.com>
2018-09-30 12:41:50 +0200

    Cut in functions.

--- cat/cat.go
+++ cat/cat.go
@@ -10,24 +10,36 @@ import (
 	"github.com/DubrulleKevin/gosix/common"
 )
 
+func readFromStdin() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func readFromFile(fileName string) {
+	fileInfo, err := os.Stat(fileName)
+	common.PanicIfError(err)
+	if !fileInfo.IsDir() {
+		fileContent, err := ioutil.ReadFile(fileName)
+		common.PanicIfError(err)
+		fmt.Print(string(fileContent))
+	}
+}
+
 func main() {
 	flag.Bool("u", false, "Not implemented")
-
 	flag.Parse()
+	args := flag.Args()
 
-	if len(flag.Args()) == 0 || common.StringSliceContains(flag.Args(), "-") {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
+	if len(args) == 0 {
+		readFromStdin()
 	} else {
-		for _, fileName := range flag.Args() {
-			fileInfo, err := os.Stat(fileName)
-			common.PanicIfError(err)
-			if !fileInfo.IsDir() {
-				fileContent, err := ioutil.ReadFile(fileName)
-				common.PanicIfError(err)
-				fmt.Print(string(fileContent))
+		for _, fileName := range args {
+			if fileName == "-" {
+				readFromStdin()
+			} else {
+				readFromFile(fileName)
 			}
 		}
 	}
